488c4d307e8adafdf8335ad9d60f5985ab2098cc
PavelOfficial <pavelofficial@ya.ru>
2016-11-11 13:30:37 +0300

    image fix

--- frontend/app/js/types/AvatarURI.js
+++ frontend/app/js/types/AvatarURI.js
@@ -25,7 +25,7 @@ types.AvatarURI.getImages = function (callback) {
             callback(types.AvatarURI.images);
         };
         xhr.onerror = function () {
-            types.AvatarURI.images = [];
+            types.AvatarURI.images = types.AvatarURI.defaultImages;
             callback(types.AvatarURI.images);
         };
 
@@ -36,7 +36,7 @@ types.AvatarURI.getImages = function (callback) {
 
 types.AvatarURI.images = null;
 
-// types.AvatarURI.images = ["https://c2.staticflickr.com/4/3117/3175014052_7484da1205_z.jpg",  "https://c2.staticflickr.com/4/3262/3175014554_db597bbb73_z.jpg",  "https://c2.staticflickr.com/4/3670/8813562512_229f5cf24a_z.jpg",  "https://c1.staticflickr.com/9/8440/7787237516_b46aa5fabb_c.jpg",  "https://c2.staticflickr.com/4/3404/3449526371_0454515b13_z.jpg",  "https://c2.staticflickr.com/4/3602/3450323658_ab5961a0aa_z.jpg",  "https://c1.staticflickr.com/3/2491/3751647375_4695b378de_z.jpg",  "https://c1.staticflickr.com/3/2443/3752426198_ebe03fa615_z.jpg",  "https://c2.staticflickr.com/2/1032/3175022066_57fce505be_z.jpg",  "https://c1.staticflickr.com/3/2528/3751624573_08815f8950_z.jpg"];
+types.AvatarURI.defaultImages = ["https://c2.staticflickr.com/4/3117/3175014052_7484da1205_z.jpg",  "https://c2.staticflickr.com/4/3262/3175014554_db597bbb73_z.jpg",  "https://c2.staticflickr.com/4/3670/8813562512_229f5cf24a_z.jpg",  "https://c1.staticflickr.com/9/8440/7787237516_b46aa5fabb_c.jpg",  "https://c2.staticflickr.com/4/3404/3449526371_0454515b13_z.jpg",  "https://c2.staticflickr.com/4/3602/3450323658_ab5961a0aa_z.jpg",  "https://c1.staticflickr.com/3/2491/3751647375_4695b378de_z.jpg",  "https://c1.staticflickr.com/3/2443/3752426198_ebe03fa615_z.jpg",  "https://c2.staticflickr.com/2/1032/3175022066_57fce505be_z.jpg",  "https://c1.staticflickr.com/3/2528/3751624573_08815f8950_z.jpg"];
 
 types.AvatarURI.prototype.isValid = function () {
     'use strict';
